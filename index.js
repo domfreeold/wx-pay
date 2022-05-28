847e4a71d359df4225561f6de305ebe3daa075d3
domfreeold <97688842+domfreeold@users.noreply.github.com>
2022-05-29 01:24:48 +0800

    Update index.js

--- index.js
+++ index.js
@@ -47,10 +47,11 @@ app.get("/api/wx_openid", async (req, res) => {
   }
 });
 app.get("/api/goPay", async (req, res) => {
+ const open_id =  req.headers['x-wx-openid'];
 const data= await new Promise((resolve, reject) => {
   request({
     method: 'POST',
-    url: `https://pay.liyishabiubiu.cn/miniapp.php?open_id=req.headers['x-wx-openid']&order_id=${req.query.order_id}&price=${req.query.price}`
+    url: `https://pay.liyishabiubiu.cn/miniapp.php?open_id=${open_id}&order_id=${req.query.order_id}&price=${req.query.price}`
   },function (error, response) {
     console.log('接口返回内容', response.body)
     resolve(JSON.parse(response.body))
